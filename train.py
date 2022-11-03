b8468ff91cf11747f600d40a9b8bbe65ec6a4a1a
Lucia <luciaurcelayg@gmail.com>
2022-11-03 15:13:25 +0100

    added documentation and updated train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+# Import Packages
+
+
+# Define main block
+if __name__ == '__main__':
+    
+    # Import data
+
+    # Preprocess data
+
+    # Create train and validation splits
+    
+    # Create model
+    
+    # Train
+    
+    pass

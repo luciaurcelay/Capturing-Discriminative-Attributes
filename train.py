b3e10acef613e899abcb6b9362e96c63096f3fa4
Lucia <luciaurcelayg@gmail.com>
2023-01-04 20:43:14 +0100

    update train.py

--- train.py
+++ train.py
@@ -10,47 +10,74 @@ from utils.argument_parser import parse_input_arguments
 from utils.path_utils import *
 from utils.csv_utils import *
 from models.svc import SVC_Trainer
+from feature_extraction.ConceptNet.conceptnet import extract_relations
 from feature_extraction.embeddings.glove_embeddings import generate_glove_embeddings
 from feature_extraction.distances import compute_l1_norm, compute_cosine_similarity
 
 # Main helper functions
-def extract_features(parsed_args, train_df, val_df, feature_names, embeddings_path):
+def extract_features(parsed_args, train_df, val_df, feature_names, embeddings_path, conceptnet_path):
     
     start = time.time()
+
+    ## Extract features from knowledge base
+    conceptnet = parsed_args.relations
+    # GloVe Embeddings
+    if conceptnet == 'True':
+
+        print('EXTRACTING CONCEPTNET RELATIONS FROM TRAINING SET')
+        # Relations from train set
+        train_df = extract_relations(train_df)
+        # Generate .csv file and save to resources folder
+        print('SAVING TRAINING RELATIONS')
+        train_df.to_csv(conceptnet_path+"/train_conceptnet_relations_0_500.csv", index=False)
+
+        print('EXTRACTING CONCEPTNET RELATIONS FROM VALIDATION SET')
+        # Relations from validation set
+        val_df = extract_relations(val_df)
+        print('SAVING VALIDATION RELATIONS')
+        val_df.to_csv(conceptnet_path+"/val_conceptnet_relations_0_500.csv", index=False)
+    
+    else:
+        pass
+
     
     ## Extract word embeddings from the attributes
     selected_embedding = parsed_args.embedding
-
     # GloVe Embeddings
     if selected_embedding == 'glove':
         
-        print('EXTRACTING FEATURES FROM TRAINING SET')
+        print('EXTRACTING GLOVE EMBEDDINGS FROM TRAINING SET')
         train_df = generate_glove_embeddings(parsed_args, train_df, feature_names, embeddings_path)
 
-        print('EXTRACTING FEATURES FROM VALIDATION SET')
+        print('EXTRACTING GOLVE EMBEDDINGS FROM VALIDATION SET')
         val_df = generate_glove_embeddings(parsed_args, val_df, feature_names, embeddings_path)
 
     # Contextual Emeddings
     elif selected_embedding == 'contextual':
 
-        print('EXTRACTING FEATURES FROM TRAINING SET')
+        print('EXTRACTING CONTEXTUAL EMBEDDINGS FROM TRAINING SET')
         # TODO
 
-        print('EXTRACTING FEATURES FROM VALIDATION SET')
+        print('EXTRACTING CONTEXTUAL FROM VALIDATION SET')
         # TODO
     
-    ## Extract features from knowledge base
-    # TODO
-
+    
     ## Compute distances
-
     # L1 norm
-    train_df = compute_l1_norm(train_df) # TODO
-    val_df = compute_l1_norm(val_df) # TODO
+    selected_l1 = parsed_args.l1_norm
+    if selected_l1 == 'True':
+        train_df = compute_l1_norm(train_df)
+        val_df = compute_l1_norm(val_df)
+    else:
+        pass
 
     # Cosine similarity
-    train_df = compute_cosine_similarity(train_df) # TODO
-    val_df = compute_cosine_similarity(val_df) # TODO
+    selected_cosine = parsed_args.cosine
+    if selected_cosine == 'True':
+        train_df = compute_cosine_similarity(train_df)
+        val_df = compute_cosine_similarity(val_df)
+    else:
+        pass
     
     end = round(time.time()-start)
     print(f'Feature extraction took {end} seconds')
@@ -63,7 +90,7 @@ def train(new_exp_path, parsed_args, train_df, val_df):
     # prepare data folds
     X_train = train_df.drop(['word1', 'word2', 'pivot', 'label'], axis=1)
     y_train = train_df['label']
-    X_val = val_df.drop(['index','word1', 'word2', 'pivot', 'label'], axis=1)
+    X_val = val_df.drop(['word1', 'word2', 'pivot', 'label'], axis=1)
     y_val = val_df['label']
     
     # print dataframes   
@@ -132,6 +159,7 @@ if __name__ == '__main__':
     experiments_paths = create_folder(root_path, 'experiments')
     new_experiment_path = create_new_experiment_folder(parsed_args, experiments_paths)
     embeddings_path = join_path(root_path, 'resources\glove.6B')
+    conceptnet_path = join_path(root_path, 'resources\conceptnet_relations')
     feature_names = ['word1', 'word2', 'pivot', 'label']
 
     # Generate random seeds
@@ -147,7 +175,7 @@ if __name__ == '__main__':
     print(f'Test dataframe:\n {test_df.head()}') """
     
     # Extract features
-    train_df_feat, val_df_feat = extract_features(parsed_args, train_df, val_df, feature_names, embeddings_path)
+    train_df_feat, val_df_feat = extract_features(parsed_args, train_df, val_df, feature_names, embeddings_path, conceptnet_path)
 
     # Train the selected classifier
     train(new_experiment_path, parsed_args, train_df_feat, val_df_feat)

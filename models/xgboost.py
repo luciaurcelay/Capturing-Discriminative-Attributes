5f69f890823e45cdedd4342afcd46823a984f0a5
Rasmus Siljander <rasmus.siljander@aalto.fi>
2023-01-05 22:47:25 +0100

    X_val data fix

--- models/xgboost.py
+++ models/xgboost.py
@@ -8,10 +8,12 @@ class XGBClassifier:
 
     def train_classifier(self, X_train, y_train, X_val) -> None:
         X_train = self.model_specific_preprocessing(X_train)
+        X_val = X_val.drop(columns=["index"])
         # Fit
         self.classifier.fit(X_train, y_train)
         # Make predictions
         predictions = self.classifier.predict(X_val)
+
         return predictions
 
     def model_specific_preprocessing(self, data):

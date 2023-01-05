0407eb12e4844d096262b69145f8094de3336242
Rasmus Siljander <rasmus.siljander@aalto.fi>
2023-01-05 22:48:06 +0100

    pandas-style l1 norm for multi-dim data

--- feature_extraction/distances.py
+++ feature_extraction/distances.py
@@ -1,4 +1,5 @@
 import math
+from itertools import combinations
 
 # Calculate the computation of l1 norm
 def calculate_l1_norm(vec1, vec2):
@@ -12,6 +13,11 @@ def calculate_l1_norm(vec1, vec2):
     else:
         norm = sum(abs(a - b) for a, b in zip(vec1, vec2))
 
+    return norm
+
+
+def calculate_l1_norm_pandas(attr1, attr2):
+    norm = abs(attr1.astype(float).values - attr2.astype(float).values).sum(axis=1)
     return norm
 
 
@@ -41,19 +47,29 @@ def compute_l1_norm(dataframe):
 
     dataframe = dataframe.assign(l1_12=None, l1_13=None, l1_23=None)
 
-    for index, value in dataframe["pivot_embedding"].iteritems():
+    attr_combinations = combinations(["word1", "word2", "pivot"], 2)
+    colnames = ["12", "13", "23"]
 
-        attribute = value
-        word1 = dataframe["word1_embedding"][index]
-        word2 = dataframe["word2_embedding"][index]
+    for colname, (attr1, attr2) in zip(colnames, attr_combinations):
+        vec1 = dataframe[[col for col in dataframe.columns if attr1 in col][1:]]
+        vec2 = dataframe[[col for col in dataframe.columns if attr2 in col][1:]]
 
-        d1_2 = calculate_l1_norm(word1, word2)
-        d1_3 = calculate_l1_norm(word1, attribute)
-        d2_3 = calculate_l1_norm(word2, attribute)
+        dataframe[f"l1_{colname}"] = calculate_l1_norm_pandas(vec1, vec2)
 
-        dataframe["l1_12"][index] = d1_2
-        dataframe["l1_13"][index] = d1_3
-        dataframe["l1_23"][index] = d2_3
+    # Still keep this for possible debugging
+    # for index, value in dataframe["pivot_embedding_dim_0"].iteritems():
+
+    #     attribute = value
+    #     word1 = dataframe["word1_embedding"][index]
+    #     word2 = dataframe["word2_embedding"][index]
+
+    #     d1_2 = calculate_l1_norm(word1, word2)
+    #     d1_3 = calculate_l1_norm(word1, attribute)
+    #     d2_3 = calculate_l1_norm(word2, attribute)
+
+    #     dataframe["l1_12"][index] = d1_2
+    #     dataframe["l1_13"][index] = d1_3
+    #     dataframe["l1_23"][index] = d2_3
 
     return dataframe
 

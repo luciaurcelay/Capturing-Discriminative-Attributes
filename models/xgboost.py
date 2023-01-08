69d99aeb91125b90014fac25fe1d5edea89573b5
Rasmus Siljander <rasmus.siljander@aalto.fi>
2023-01-09 00:56:31 +0100

    parameter handling for xgboost

--- models/xgboost.py
+++ models/xgboost.py
@@ -3,8 +3,17 @@ import pandas as pd
 
 
 class XGBClassifier:
-    def __init__(self) -> None:
-        self.classifier = xgb.XGBClassifier()
+    def __init__(self, eta, n_estimators=100, max_depth=6, colsamp_bytree=1) -> None:
+        self.classifier = xgb.XGBClassifier(
+            verbosity=0,
+            eta=eta,
+            n_estimators=n_estimators,
+            max_depth=max_depth,
+            colsample_bytree=colsamp_bytree,
+        )
+        self.params = self.create_params_dict(
+            eta, n_estimators, max_depth, colsamp_bytree
+        )
 
     def train_classifier(self, X_train, y_train, X_val) -> None:
         X_train = self.model_specific_preprocessing(X_train)
@@ -19,3 +28,8 @@ class XGBClassifier:
     def model_specific_preprocessing(self, data):
         data = data.astype(float)
         return data
+
+    def create_params_dict(self, eta, n_estimators, max_depth, colsamp_bytree):
+        param_names = ["eta", "n_estimators", "max_depth", "colsamp_bytree"]
+        params = [eta, n_estimators, max_depth, colsamp_bytree]
+        return dict(zip(param_names, params))
